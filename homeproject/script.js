cdae9b220be162f90e4dbbbd601f604f3fedd7d5
Falkovic1993 <31372909+Falkovic1993@users.noreply.github.com>
2017-09-17 10:09:36 +0200

    Add files via upload
    Uploaded files from Local.

---
+++ homeproject/script.js
@@ -0,0 +1,93 @@
+	// JAVASCIPT! 
+console.log("It's working!");
+
+
+//  NAVIGATION - 1 PAGE ONLY SETUP. 
+	var aBtnShowPage = document.getElementsByClassName('btnMenu');
+	var hidePage = document.getElementsByClassName('page')
+
+	for (var i = 0; i < aBtnShowPage.length; i++) {
+		aBtnShowPage[i].addEventListener("click", function() {
+		//console.log(hidePage)
+		for (var j = 0; j < hidePage.length; j++) {
+			hidePage[j].style.display = "none";
+			}
+		var sDataAttribute = this.getAttribute("data-showThisPage");
+		//console.log(sDataAttribute);
+		document.getElementById(sDataAttribute).style.display = "flex";
+			});
+		};
+
+
+// SHOW THE SIGN UP FORM
+
+	btnSignUpForm.addEventListener("click", function(){
+		console.log("X")
+		SignUpBox.style.display = "flex";
+	});
+
+// CLOSE THE SIGN UP FORM
+
+	closeSignUp.addEventListener("click", function(){
+		SignUpBox.style.display = "none";
+	})
+
+// SAVE THE USER INTO OUR DATA WHEN THEY SIGN UP 
+
+	btnUserSU.addEventListener("click", function(){
+			console.log("X")
+
+			var ajax = new XMLHttpRequest();
+			ajax.onreadystatechange = function() {
+				if(this.readyState == 4 && this.status == 200) {
+				  	var sDataFromServer = JSON.parse(this.responseText);
+         			console.log("Response: ",sDataFromServer);
+         			var $name = txtUserNameSU.value; 
+         			console.log(name);
+				
+
+				}
+			}
+			ajax.open( "POST", "api-signup.php", true );
+		    var jFrmLogin = new FormData(frmSignUpUser);
+		    ajax.send(frmSignUpUser);
+		});
+
+	
+
+// LOGIN TO THE SITE! 
+
+	btnLogin.addEventListener("click", function(){
+		console.log("Z")
+
+		var ajax = new XMLHttpRequest();
+		ajax.onreadystatechange = function() {
+
+			if(this.readyState == 4 && this.status == 200) {
+			var jDataFromServer = JSON.parse( this.responseText );
+			console.log(jDataFromServer);
+			btnLogOut.style.display = "flex";
+
+			}
+		}
+		ajax.open( "POST", "api-login.php", true );
+	    var jFrmLogin = new FormData(frmLogin);
+	    ajax.send(jFrmLogin);
+	});
+
+// LOGOUT OF THE SITE 
+
+	btnLogOut.addEventListener("click", function(){
+		console.log("LogOut")
+
+		var ajax = new XMLHttpRequest();
+		ajax.onreadystatechange = function() {
+
+			if (this.readyState == 4 && this.status == 200) {
+				window.location.reload();
+			}
+		}
+		ajax.open( "GET", "api-logout.php", true);
+		ajax.send();
+
+	})
